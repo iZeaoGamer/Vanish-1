fbf36073dd401d82cb6231f7ccd70af5c72a7ed7
iZeaoGamer <jameshayward14@icloud.com>
2018-03-31 15:02:10 +0100

    Update Main.php

--- Vanish/Main.php
+++ Vanish/Main.php
@@ -18,14 +18,14 @@ $this->getServer()->getPluginManager()->registerEvents($this,$this);
    }
 
 public function onCommand(CommandSender $sender,Command $cmd,$label,array $args) : bool {
-if($cmd->getName() == "vanish"){
+if($cmd->getName() == "sv"){
 if($sender instanceof Player){
-if($sender->hasPermission("vanish.command")){
+if($sender->hasPermission("supervanish.spectate")){
           if(isset($args[0])){
     if($args[0] == "on"){
  $sender->setNameTagVisible(false);
  $sender->addEffect(Effect::getEffect(Effect::INVISIBILITY)->setDuration(99999999*20)->setAmplifier(1)->setVisible(false));
- $event->getPlayer()->addTitle("§l§7Vanish has been", "§l§7Turned - §aON", 40, 100, 40);
+ $event->getPlayer()->addTitle("§a§lVanish has been", "§l§bTurned - §aON", 40, 100, 40);
         }
    if($args[0] == "off"){
  $sender->setNameTagVisible(true);
@@ -35,7 +35,7 @@ if($sender->hasPermission("vanish.command")){
           $sender->sendMessage("§l§bVanish §8> §r§aUse /vanish <on|off>");
      }
    }else{
-    $sender->sendMessage("- you must have a premium rank in order to use this command");
+    $sender->sendMessage("§c- you must be staff in order to use this command");
       }
   }else{
     $sender->sendMessage("Please use this command in game");
